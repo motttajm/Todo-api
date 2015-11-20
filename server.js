7dd9455d5cba3375a10071e3791905d4490a84f4
Jim Motta <jim.motta@gmail.com>
2015-11-20 07:04:23 -0800

    Add PUT Todos

--- server.js
+++ server.js
@@ -53,12 +53,37 @@ app.delete('/todos/:id', function (req,res) {
 		todos = _.without(todos,matchedTodo);
 		res.json(matchedTodo);
 	} else{
-		res.status(404).send({"error": "No todo found with this id"});
+		res.status(404).send({"error": "ID not found"});
 	}
 });
 
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var updateID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: updateID});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
 
+	if (!matchedTodo) {
+		res.status(404).send({"error": "ID not found"});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send({"error": "completed data bad"});
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send({"error": "description data bad"});
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
 
 //start app
 app.listen(process.env.PORT || 3000, function () {
