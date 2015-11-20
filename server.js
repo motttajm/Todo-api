55bb15ed22cc9e24e954ffb2a2bc03cc118af21d
Jim Motta <jim.motta@gmail.com>
2015-11-20 06:26:33 -0800

    added DELETE /todos/:id

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 //GET
-
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+//GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoID = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoID});
@@ -30,22 +31,47 @@ app.get('/todos/:id', function (req, res) {
 	}
 });
 
-//POST
-
+//POST /todos
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).Send();
 	}
-
 	body.description = body.description.trim();
-
 	body.id = todoNextId++;	
 	todos.push(body);
 	res.json(body);
 });
 
+
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req,res) {
+	var deletionID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: deletionID});
+
+	if(matchedTodo){
+		todos = _.without(todos,matchedTodo);
+		res.json(matchedTodo);
+	} else{
+		res.status(404).send({"error": "No todo found with this id"});
+	}
+});
+
+
+
+
+//start app
 app.listen(process.env.PORT || 3000, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
