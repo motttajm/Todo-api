dbc9e5135110bbb8e0ece0fec639098ba8271741
Jim Motta <jim.motta@gmail.com>
2015-11-20 12:12:49 -0800

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: "Andrew",
+// 	age: 21
+// };
+
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+
+//ARRAY example
+var grades = [5,44];
+
+function addGrades (gradesArr, newGrade) {
+	gradesArr.push(newGrade);
+	debugger;
+}
+
+addGrades(grades,99);
+console.log(grades);
