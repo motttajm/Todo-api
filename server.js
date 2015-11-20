a64dd2440b8346b65f237e76a4e45b0479f629f9
Jim Motta <jim.motta@gmail.com>
2015-11-20 12:11:56 -0800

    Add Completed filter

--- server.js
+++ server.js
@@ -14,9 +14,18 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-//GET /todos
+//GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	res.json(filteredTodos);
 });
 
 //GET /todos/:id
