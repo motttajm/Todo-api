2ec30cb794420824de8f5e94fffb4aee66fbc5f2
Jim Motta <jim.motta@gmail.com>
2015-11-23 08:56:33 -0800

    add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -58,13 +60,23 @@ app.get('/todos/:id', function(req, res) {
 //POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).Send();
-	}
-	body.description = body.description.trim();
-	body.id = todoNextId++;
-	todos.push(body);
-	res.json(body);
+
+
+	db.todo.create(body).then(function (todo) {
+		//todos.push(todo);
+		//res.status(200).send();
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).Send();
+	// }
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
+	// todos.push(body);
+	// res.json(body);
 });
 
 
@@ -121,7 +133,10 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-//start app
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express listening on port ' + PORT + '!');
+//sync database
+db.sequelize.sync().then(function() {
+	//start app
+	app.listen(process.env.PORT || 3000, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
