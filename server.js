cc83cb64f049ad9e93fc22a58808aac2a6496cfe
Jim Motta <jim.motta@gmail.com>
2015-11-23 10:06:58 -0800

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -46,35 +46,27 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoID
-	});
 
-	if (matchedTodo) {
-		res.send(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoID).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		}else {
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	})
 });
 
 //POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-
-	db.todo.create(body).then(function (todo) {
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function (e) {
-		res.status(400).json(e);
+	}, function(e) {
+		res.status(500).json(e);
 	});
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).Send();
-	// }
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-	// todos.push(body);
-	// res.json(body);
 });
 
 
