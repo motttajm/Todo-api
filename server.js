f05e85dbb0797bc63858d17e8370e67a395d3915
Jim Motta <jim.motta@gmail.com>
2015-11-25 19:20:08 -0800

    convert PUT to use sequelize

--- server.js
+++ server.js
@@ -36,9 +36,11 @@ app.get('/todos', function(req, res) {
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function (todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	}, function (e) {
+	}, function(e) {
 		res.status(500).send();
 	})
 });
@@ -50,7 +52,7 @@ app.get('/todos/:id', function(req, res) {
 	db.todo.findById(todoID).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
-		}else {
+		} else {
 			res.status(404).send();
 		}
 	}, function(e) {
@@ -72,12 +74,11 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var deletionID = parseInt(req.params.id, 10);
-
 	db.todo.destroy({
 		where: {
 			id: deletionID
 		}
-	}).then (function (rowsDeleted) {
+	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
 			res.status(404).json({
 				error: 'No todo with id'
@@ -85,7 +86,7 @@ app.delete('/todos/:id', function(req, res) {
 		} else {
 			res.status(204).send();
 		}
-	}, function () {
+	}, function() {
 		res.status(500).send();
 	})
 });
@@ -94,36 +95,36 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var updateID = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: updateID
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		res.status(404).send({
-			"error": "ID not found"
-		});
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send({
-			"error": "completed data bad"
-		});
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send({
-			"error": "description data bad"
-		});
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(updateID).then(function(todo) {
+		if (todo) {
+			//findByID successful
+			todo.update(attributes).then(function(todo) {
+				//follow-up for todo.update
+				//update succeeded
+				res.json(todo.toJSON());
+			}, function(e) {
+				//update failed
+				res.status(400).json(e);
+			});
+		} else {
+			//finById successful but id was not found
+			res.status(404).send();
+		}
+	}, function() {
+		//findByID fails
+		res.status(500).send();
+	});
 });
 
 //sync database
