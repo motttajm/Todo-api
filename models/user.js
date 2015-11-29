da794d170f59654a14b3a80f84900cd2e468846b
Jim Motta <jim.motta@gmail.com>
2015-11-29 08:55:17 -0800

    add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -62,6 +62,29 @@ module.exports = function(sequelize, DataTypes) {
 						return reject();
 					});
 				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function(user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function(e) {
+							console.error(e);
+							reject();
+						});
+					} catch (e) {
+						console.error(e);
+						reject();
+					}
+				});
 			}
 		},
 		instanceMethods: {
@@ -69,13 +92,16 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
 			},
-			generateToken: function (type) {
+			generateToken: function(type) {
 				if (!_.isString(type)) {
 					return undefined;
 				}
 
 				try {
-					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@!').toString();
 					var token = jwt.sign({
 						token: encryptedData
