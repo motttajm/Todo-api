f743be29d020d1873034761b9f65d2af931d1f77
Jim Motta <jim.motta@gmail.com>
2015-11-29 17:13:38 -0800

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get('/', function(req, res) {
 //GET /todos?completed=true&q=house
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
-
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	//filter for completed
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
@@ -50,7 +51,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoID,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -67,8 +73,8 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.create(body).then(function(todo) {
 		//success
-		req.user.addTodo(todo).then(function () {
-			return todo.reload().then(function (todo) {
+		req.user.addTodo(todo).then(function() {
+			return todo.reload().then(function(todo) {
 				res.json(todo.toJSON());
 			})
 		});
@@ -80,9 +86,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var deletionID = parseInt(req.params.id, 10);
+
 	db.todo.destroy({
 		where: {
-			id: deletionID
+			id: deletionID,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -111,7 +119,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(updateID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoID, 
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			//findByID successful
 			todo.update(attributes).then(function(todo) {
